0bbb99db0b0361323a2d956a59b01c8928868fc4
rjm099 <rihamsadouk0@gmail.com>
2026-09-06 19:57:20 -0400

    Remove time display functionality from start.py

--- start.py
+++ start.py
@@ -1,11 +1,1 @@
-import datetime
-
-# 1. Ask your computer for the exact time right now
-current_time = datetime.datetime.now()
-
-# 2. Format it to look like a normal digital clock (e.g., 07:28 PM)
-clock_display = current_time.strftime("%I:%M %p")
-
-# 3. Print it to the screen
-print(f"COMPOWDER SYSTEM ONLINE. Local Time: {clock_display}")
 
